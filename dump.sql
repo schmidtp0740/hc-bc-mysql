bb016cbf09a76c0db7cb4725f89efaba7ed70913
schmidtp0740 <schmidtp0740@gmail.com>
2018-12-14 15:26:05 -0600

    changed timestamp to approved for constraint

--- dump.sql
+++ dump.sql
@@ -3,5 +3,6 @@ CREATE TABLE rxlist (
   rxid VARCHAR(5) NOT NULL,
   timestamp BIGINT NOT NULL,
   status VARCHAR(10) NOT NULL,
-  CONSTRAINT UC_rx UNIQUE (patientID, rxid, timestamp)
+  approved VARCHAR(10) NOT NULL,
+  CONSTRAINT UC_rx UNIQUE (patientID, rxid, status, approved)
 );
